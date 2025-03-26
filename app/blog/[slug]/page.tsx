5c02fd4147aa844641722bde80142dc62251540c
unknown <raiyanrahman78@gmail.com>
2025-03-26 08:15:54 +0600

    m

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,10 @@
 import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import { formatDate, getAllBlogPosts } from 'app/blog/utils'
 import { baseUrl } from 'app/sitemap'
 
 export async function generateStaticParams() {
-  let posts = getBlogPosts()
+  let posts = getAllBlogPosts()
 
   return posts.map((post) => ({
     slug: post.slug,
@@ -12,7 +12,7 @@ export async function generateStaticParams() {
 }
 
 export function generateMetadata({ params }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug)
+  let post = getAllBlogPosts().find((post) => post.slug === params.slug)
   if (!post) {
     return
   }
@@ -52,7 +52,7 @@ export function generateMetadata({ params }) {
 }
 
 export default function Blog({ params }) {
-  const post = getBlogPosts().find((post) => post.slug === params.slug)
+  const post = getAllBlogPosts().find((post) => post.slug === params.slug)
 
 
   if (!post) {
