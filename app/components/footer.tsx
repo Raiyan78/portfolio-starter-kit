d47cbabc66c5e2aca3587ce4600cc021e1dec9c5
unknown <raiyanrahman78@gmail.com>
2025-03-26 22:16:09 +0600

    m

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,21 +1,40 @@
-function ArrowIcon() {
+"use client";
+
+import { useEffect, useState } from 'react';
+
+export default function Footer() {
+  const [lastEdited, setLastEdited] = useState('');
+
+  useEffect(() => {
+    const lastModified = new Date(document.lastModified);
+    setLastEdited(lastModified.toLocaleDateString());
+  }, []);
+
   return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-        stroke="currentColor"
-        strokeWidth="0.5"
-      />
-    </svg>
+    <footer className="mb-16 text-center text-neutral-600 dark:text-neutral-300">
+      <p>Last edited on {lastEdited}</p>
+      <p>Built with Next.js</p>
+    </footer>
   );
 }
+// function ArrowIcon() {
+//   return (
+//     <svg
+//       width="12"
+//       height="12"
+//       viewBox="0 0 12 12"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <path
+//         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+//         fill="currentColor"
+//         stroke="currentColor"
+//         strokeWidth="0.5"
+//       />
+//     </svg>
+//   );
+// }
 
 // export default function Footer() {
 //   return (
@@ -61,21 +80,3 @@ function ArrowIcon() {
 //     </footer>
 //   );
 // }
-import { useEffect, useState } from 'react';
-
-export default function Footer() {
-  const [lastEdited, setLastEdited] = useState('');
-
-  useEffect(() => {
-    // Get the document's last modified date and format it as needed
-    const lastModified = new Date(document.lastModified);
-    setLastEdited(lastModified.toLocaleDateString());
-  }, []);
-
-  return (
-    <footer className="mb-16 text-center text-neutral-600 dark:text-neutral-300">
-      <p>Last edited on {lastEdited}</p>
-      <p>Built with Next.js</p>
-    </footer>
-  );
-}
