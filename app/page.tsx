060766c4cb416e6b5c20d30dac4bcb533929d0ba
unknown <raiyanrahman78@gmail.com>
2025-03-26 08:37:58 +0600

    m

--- app/page.tsx
+++ app/page.tsx
@@ -23,19 +23,21 @@ export default function Page() {
 
         {/* About Text */}
         <div>
-        <p>
-        I am currently working as a research assistant at ECE, North South University, under the supervision of  
-        <a href="https://northsouth.edu/faculty-members/seps/mathematics-physics/dr.-abul-kalam-al-azad-akad.html"><strong> Prof. Abul Kalam al Azad</strong></a> &  
-        <a href="https://ece.northsouth.edu/people/dr-sifat-momen/"><strong> Prof. Sifat Momen</strong></a> 
-        <br /><br />
-        My research interest lies in the realm of quantum cryptography, 
-        where I envision to build an efficient and information theoretically secure algorithm. Previously, I have
-        worked closely with  
-        <a href="https://sites.google.com/view/nsuopticslab/home"><strong> Prof. Mahdy Rahman Chowdhury</strong></a>.
-        Ultimately, my goal is to build a world where privacy is a guaranteed facet in everyone's life.
-      </p>
+          <p>
+            I am currently working as a research assistant at ECE, North South University, under the supervision of  
+            <a href="https://northsouth.edu/faculty-members/seps/mathematics-physics/dr.-abul-kalam-al-azad-akad.html"><strong> Prof. Abul Kalam al Azad</strong></a> &  
+            <a href="https://ece.northsouth.edu/people/dr-sifat-momen/"><strong> Prof. Sifat Momen</strong></a> 
+            <br /><br />
+            My research interest lies in the realm of quantum cryptography, 
+            where I envision to build an efficient and information theoretically secure algorithm. Previously, I have
+            worked closely with  
+            <a href="https://sites.google.com/view/nsuopticslab/home"><strong> Prof. Mahdy Rahman Chowdhury</strong></a>.
+            Ultimately, my goal is to build a world where privacy is a guaranteed facet in everyone's life.
+          </p>
 
         </div>
+        Email: raiyanrahman78 [at] gmail [dot] com
+        <br />
       </div>
 
       {/* Blog Posts */}
