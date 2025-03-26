c3a698a909b15fd0f44104cd2db7a08c911af2f8
unknown <raiyanrahman78@gmail.com>
2025-03-26 21:55:59 +0600

    m

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -51,31 +51,82 @@ import { formatDate, getBlogPosts, getArticles } from 'app/blog/utils'
 //   )
 // }
 
+// export function BlogPosts() {
+//   let allBlogs = getBlogPosts()
+
+//   return (
+//     <div>
+//       {allBlogs
+//         .sort((a, b) => new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime())
+//         .map((post) => (
+//           <Link
+//             key={post.slug}
+//             className="flex flex-col space-y-1 mb-4"
+//             href={`/blog/${post.slug}`}
+//           >
+//             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-start">
+//               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+//                 {formatDate(post.metadata.publishedAt, false)}
+//               </p>
+
+//               <div className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+//                 <p className="font-medium mt-1">{post.metadata.title}</p>
+
+//                 {post.metadata.authors && (
+//                   <p className="text-sm text-neutral-700 dark:text-neutral-300 mt-1">
+//                     <strong>Authors:</strong>{' '}
+//                     {post.metadata.authors.split(/\s*,\s*/).map((author, index, array) => {
+//                       const isMe = author.trim() === 'Raiyan Rahman'
+//                       return (
+//                         <span key={index}>
+//                           {isMe ? <u>{author}</u> : author}
+//                           {index < array.length - 1 ? ', ' : ''}
+//                         </span>
+//                       )
+//                     })}
+//                   </p>
+//                 )}
+//               </div>
+
+//             </div>
+
+//           </Link>
+//         ))}
+//     </div>
+//   )
+// }
+
+
 export function BlogPosts() {
   let allBlogs = getBlogPosts()
 
   return (
-    <div>
+    <div className="flex flex-col gap-y-4">
       {allBlogs
-        .sort((a, b) => new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime())
+        .sort(
+          (a, b) =>
+            new Date(b.metadata.publishedAt).getTime() -
+            new Date(a.metadata.publishedAt).getTime()
+        )
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col md:flex-row space-x-0 md:space-x-2 items-start"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-start">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
+            <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+              {formatDate(post.metadata.publishedAt, false)}
+            </p>
 
-              <div className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                <p className="font-medium mt-1">{post.metadata.title}</p>
+            <div className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="font-medium mt-1">{post.metadata.title}</p>
 
-                {post.metadata.authors && (
-                  <p className="text-sm text-neutral-700 dark:text-neutral-300 mt-1">
-                    <strong>Authors:</strong>{' '}
-                    {post.metadata.authors.split(/\s*,\s*/).map((author, index, array) => {
+              {post.metadata.authors && (
+                <p className="text-sm text-neutral-700 dark:text-neutral-300 mt-1">
+                  <strong>Authors:</strong>{' '}
+                  {post.metadata.authors
+                    .split(/\s*,\s*/)
+                    .map((author, index, array) => {
                       const isMe = author.trim() === 'Raiyan Rahman'
                       return (
                         <span key={index}>
@@ -84,12 +135,9 @@ export function BlogPosts() {
                         </span>
                       )
                     })}
-                  </p>
-                )}
-              </div>
-
+                </p>
+              )}
             </div>
-
           </Link>
         ))}
     </div>
@@ -127,7 +175,7 @@ export function Articles() {
                 
                 {post.metadata.authors && post.metadata.authors.length > 0 && (
                     <p className="text-sm text-neutral-700 dark:text-neutral-300 mt-1">
-                      <strong>Authors:</strong>{' '}
+                      {/* <strong>Authors:</strong>{' '} */}
                       {post.metadata.authors.split(/\s*,\s*/).map((author, index, array) => {
                         const isMe = author.trim() === 'Raiyan Rahman'
                         return (
