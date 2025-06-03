210467d6ae82e666e71e05b75b63e7cd28f3afc7
unknown <raiyanrahman78@gmail.com>
2025-06-04 03:27:34 +0600

    updated tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -114,15 +114,18 @@ export function BlogPosts() {
             href={`/blog/${post.slug}`}
             className="grid grid-cols-[100px_1fr] gap-x-4"
           >
-            <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+            <p className="text-neutral-800 dark:text-neutral-400 tabular-nums">
+
               {formatDate(post.metadata.publishedAt, false)}
             </p>
 
-            <div className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+            <div className="text-neutral-950 dark:text-neutral-100 tracking-tight">
+
               <p className="font-medium">{post.metadata.title}</p>
 
               {post.metadata.authors && (
-                <p className="text-sm text-neutral-700 dark:text-neutral-300 mt-1">
+                <p className="text-sm text-neutral-800 dark:text-neutral-300 mt-1">
+
                   {/* <strong>Authors:</strong>{' '} */}
                   {post.metadata.authors
                     .split(/\s*,\s*/)
@@ -168,14 +171,17 @@ export function Articles() {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+              <p className="text-neutral-800 dark:text-neutral-400 w-[100px] tabular-nums">
+
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="text-neutral-950 dark:text-neutral-100 tracking-tight">
+
                 {post.metadata.title}
                 
                 {post.metadata.authors && post.metadata.authors.length > 0 && (
-                    <p className="text-sm text-neutral-700 dark:text-neutral-300 mt-1">
+                    <p className="text-sm text-neutral-800 dark:text-neutral-300 mt-1">
+
                       {/* <strong>Authors:</strong>{' '} */}
                       {post.metadata.authors.split(/\s*,\s*/).map((author, index, array) => {
                         const isMe = author.trim() === 'Raiyan Rahman'
