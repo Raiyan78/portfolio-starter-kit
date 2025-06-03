9b4614e045436f0c1a96b318f06018358e4d466f
unknown <raiyanrahman78@gmail.com>
2025-06-04 03:46:46 +0600

    m

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-export default function Footer() {
+export function Footer() {
   const [lastEdited, setLastEdited] = useState('');
 
   useEffect(() => {
