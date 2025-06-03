62c016fcdc0e60bd76266aa07526fd411a416de0
unknown <raiyanrahman78@gmail.com>
2025-06-04 03:37:23 +0600

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,8 +56,8 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        //'text-black bg-white dark:text-white dark:bg-black',
-        'text-black bg-white',
+        'text-black bg-white dark:text-white dark:bg-black',
+        //'text-black bg-white',
         playfair.variable,
       )}
     >
